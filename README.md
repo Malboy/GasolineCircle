e434ae2a25c980cd050e959ba8f7aeacfbe5edab
Malboy <borzyhanton@gmail.com>
2021-05-21 17:38:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GasolineCircle
+
+Developed with Unreal Engine 4
---
+++ Source/GasolineCircle/GasolineCircleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GasolineCircleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGasolineCircleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGasolineCircleGameMode();
+};
+
+
+
